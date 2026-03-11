0283b1074e7ea12d8bf3fd496f37a13c1a5220b1
Konrad-Rejman <u09kr22@abdn.ac.uk>
2026-03-11 13:55:57 +0000

    Changed Equity function

--- main.py
+++ main.py
@@ -46,50 +46,61 @@ class MyPlayer(Player):
             7462: 1 # High Card
         }
 
-        rank = rankings[self.get_hand_type(community_cards)]
-
-        card_values = {
-            '2': 2,
-            '3': 3,
-            '4': 4,
-            '5': 5,
-            '6': 6,
-            '7': 7,
-            '8': 8,
-            '9': 9,
-            'T': 10,
-            'J': 11,
-            'Q': 12,
-            'K': 13,
-            'A': 14
-        }
-
-        rank *= max(card_values[self.cards[0][0]], card_values[self.cards[1][0]])
-
-        return rank
+        equity = rankings[self.get_hand_type(community_cards)]
+        return equity / 7462 # Normalise equity to 0.0 - 1.0
 
     def move(self, community_cards: list[str], valid_moves: list[Move], round_history: list[tuple[Move, int]], min_bet: int, max_bet: int) -> tuple[Move, int] | Move:
         """Your move code here! You are given the community cards (cards both players have access to, the objective is to use your 2 cards (self.cards) with the community cards to make the best 5-card poker hand).
         You are also given a list containing the legal moves you can currently make, for example, if the opponent has bet then you can only call, raise or fold but cannot check.
         If your bot attempts to make an illegal move it will fold its hand (forfeiting any chips already in the pot), so ensure not to do this."""
         
-        # self.get_hand_type(community_cards) == HandRank.THREE_OF_A_KIND
-        rank = self.get_equity(community_cards)
-
         # Adjust weights according to hand strength and history
-        weights = {Move.CHECK: 100, Move.CALL: 100, Move.BET: 100, Move.RAISE: 100, Move.ALL_IN: 100, Move.FOLD: 100}
 
-        weights[Move.ALL_IN] = weights[Move.ALL_IN] * rank
-        weights[Move.FOLD] = weights[Move.FOLD] / rank
+        # Size of pot to bet for
+        pot = 0
+        for r in round_history:
+            pot += r[1]
 
-        weights = [v for k, v in weights.items() if k in valid_moves]
+        # Adjust weights based off hand strength
+        equity = self.get_equity(community_cards)
+
+        if equity < 0.33:
+            weights = {
+                Move.CHECK: 100,
+                Move.CALL: 40,
+                Move.BET: 5,
+                Move.RAISE: 2,
+                Move.ALL_IN: 0,
+                Move.FOLD: 80
+            }
+
+        elif equity < 0.66:
+            weights = {
+                Move.CHECK: 60,
+                Move.CALL: 100,
+                Move.BET: 30,
+                Move.RAISE: 20,
+                Move.ALL_IN: 5,
+                Move.FOLD: 20
+            }
+
+        else:
+            weights = {
+                Move.CHECK: 20,
+                Move.CALL: 80,
+                Move.BET: 80,
+                Move.RAISE: 60,
+                Move.ALL_IN: 25,
+                Move.FOLD: 1
+            }
 
         # Choose random move according to the weights
+        weights = [weights[k] for k in valid_moves]
         move = random.choices(valid_moves, weights=weights, k=1)[0]
         if move == Move.RAISE:
             return (Move.RAISE, 100)
         elif move == Move.BET:
-            return (Move.BET, random.choice([range(min_bet, max_bet)]))
+            return (Move.BET, min_bet)
         return move
 
 def run_match(_: int) -> str:

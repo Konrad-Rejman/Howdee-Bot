09bfea716cf50c9838ae9773a2c79927f0cebfad
Konrad-Rejman <u09kr22@abdn.ac.uk>
2026-03-11 14:31:03 +0000

    Re-added hand value to equity formula

--- main.py
+++ main.py
@@ -46,8 +46,25 @@ class MyPlayer(Player):
             7462: 1 # High Card
         }
 
+        card_values = {
+            '2': 2,
+            '3': 3,
+            '4': 4,
+            '5': 5,
+            '6': 6,
+            '7': 7,
+            '8': 8,
+            '9': 9,
+            'T': 10,
+            'J': 11,
+            'Q': 12,
+            'K': 13,
+            'A': 14
+        }
+
         equity = rankings[self.get_hand_type(community_cards)]
-        return equity / 7462 # Normalise equity to 0.0 - 1.0
+        equity *= max(card_values[self.cards[0][0]], card_values[self.cards[1][0]])
+        return equity / (7462 * 14) # Normalise equity to 0.0 - 1.0
 
     def move(self, community_cards: list[str], valid_moves: list[Move], round_history: list[tuple[Move, int]], min_bet: int, max_bet: int) -> tuple[Move, int] | Move:
         """Your move code here! You are given the community cards (cards both players have access to, the objective is to use your 2 cards (self.cards) with the community cards to make the best 5-card poker hand).
@@ -88,23 +105,28 @@ class MyPlayer(Player):
             }
         
         # Choose a random personality for the hand, to confuse aggression detection
-        personality = random.choice([0, 1]) # 0 = passive, 1 = aggressive
-        if personality == 0:
-            weights[Move.CALL] *= 0.33
-            weights[Move.BET] *= 0.33
-            weights[Move.ALL_IN] *= 0.33
-            weights[Move.FOLD] *= 3
+        if not hasattr(self, 'personality'):
+            self.personality = random.choice([0, 1]) # 0 = passive, 1 = aggressive
+
+        if self.personality == 0:
+            weights[Move.BET] *= 0.5
+            weights[Move.RAISE] *= 0.4
+            weights[Move.ALL_IN] *= 0.2
+            weights[Move.FOLD] *= 2
+
         else:
-            weights[Move.CALL] *= 3
-            weights[Move.BET] *= 3
-            weights[Move.ALL_IN] *= 3
-            weights[Move.FOLD] *= 0.33
+            weights[Move.BET] *= 2
+            weights[Move.RAISE] *= 2
+            weights[Move.ALL_IN] *= 1.5
+            weights[Move.FOLD] *= 0.5
         
         # Adjust bet based on pot size and equity
-        pot = 0
-        for r in round_history:
-            pot += r[1]
-        bet_amount = max(min_bet, min(pot // 2, max_bet))
+        pot = sum(amount for _, amount in round_history)
+
+        spread = max_bet - min_bet
+        bet_amount = int(min_bet + spread * equity)
+        bet_amount = int((bet_amount + pot * equity) / 2)
+        bet_amount = max(min_bet, min(bet_amount, max_bet))
 
         # Choose random move according to the weights
         weights = [weights[k] for k in valid_moves]
